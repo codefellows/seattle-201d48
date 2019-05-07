fd07c0baee2b2affd0528fc097b9a47dfbd3ffb4
Sam Hamm <sam@codefellows.com>
2019-05-07 12:27:33 -0700

    Uploads tables demo

--- class-07/demo-tables/app.js
+++ class-07/demo-tables/app.js
@@ -33,6 +33,24 @@ function Cat(name, color, tailSize) {
   this.name = name;
   this.color = color;
   this.tailSize = tailSize;
+  this.render = function () {
+    // make a tr
+    var trEl = document.createElement('tr');
+    // create, content, append for "Name"
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.name;
+    trEl.appendChild(tdEl);
+    // create, content, append for "Color"
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.color;
+    trEl.appendChild(tdEl);
+    // create. content, append for "Tail Size"
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.tailSize;
+    trEl.appendChild(tdEl);
+    // append the tr to the table
+    catTable.appendChild(trEl);
+  };
   allCats.push(this);
 }
 
@@ -46,26 +64,41 @@ new Cat('Meow Mix', 'Black', 'Medium');
 new Cat('Toeny', 'Gray', 'Medium-large');
 new Cat('Diva', 'White', 'Fluffy');
 
-console.table(allCats);
-
-Cat.prototype.render = function() {
-  // make a tr
-  // create, content, append for "Name"
-  // create, content, append for "Color"
-  // create. content, append for "Tail Size"
-  // append the tr to the table
-};
-
+// console.table(allCats);
 
 // We need a separate function to make the table header
 
-function makeHeaderRow() {}
+function makeHeaderRow() {
+  // create the row
+  var trEl = document.createElement('tr');
+  // create, content, append first cell
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Name';
+  trEl.appendChild(thEl);
+  // create, content, append second cell
+  thEl = document.createElement('th');
+  thEl.textContent = 'Color';
+  trEl.appendChild(thEl);
+  // create, content, append third cell
+  thEl = document.createElement('th');
+  thEl.textContent = 'Tail Size';
+  trEl.appendChild(thEl);
+  // append the row to the table
+  catTable.appendChild(trEl);
+}
 
 
 
 // It would be nice to have a single function that renders all of the individual cat rows
-function renderAllCats() {}
+function renderAllCats() {
+  for (var i = 0; i < allCats.length; i++) {    
+    allCats[i].render();
+  }
+}
 
 // Now we need to call our functions: the one for the header row, and the one for generating the individual cat rows
+
+makeHeaderRow();
+renderAllCats();
 
 // Don't forget in the Chrome dev tools to observe the difference between the HTML shown in the Sources tab versus the Elements tab!
